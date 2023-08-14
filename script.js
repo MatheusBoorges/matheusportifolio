22781b908c416111d1b6c2c755c7962349bb8ea5
Matheus Borges <mateusbborges4@gmail.com>
2023-08-13 21:40:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('.area-1', {rotate: {x: 80, y: 0, z: 2} ,duration: 1000});
+sr.reveal('.area-2', {rotate: {x: 80, y: 0, z: 0} ,duration: 1000});
+sr.reveal('.area-3', {rotate: {x: 80, y: 0, z: 0} ,duration: 1000});
+sr.reveal('.area-4', {rotate: {x: 80, y: 0, z: 0} ,duration: 1000});
